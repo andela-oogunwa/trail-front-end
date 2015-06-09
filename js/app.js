c24744de1a85fa425c1bd0981912758208d66d50
andela-oogunwa <fadekemi.ogunwa@andela.co>
2015-06-09 16:32:19 +0100

    implement filter for members

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ angular.module('TrailApp', ['ngMaterial']);
 
 angular.module('TrailApp').controller('MainCtrl',['$scope','$timeout','$mdSidenav', '$mdUtil', function($scope, $timeout, $mdSidenav, $mdUtil){
   $scope.toggleLeft = buildToggler('left');
+  $scope.filterValue = 'Nad Enegesi';
   function buildToggler(navID) {
     var debounceFn =  $mdUtil.debounce(function(){
           $mdSidenav(navID)
@@ -15,25 +16,34 @@ angular.module('TrailApp').controller('MainCtrl',['$scope','$timeout','$mdSidena
   $scope.close = function () {
     $mdSidenav('left').close();
   };
+
+  $scope.filter = function(value) {
+    $scope.filterValue = value;
+  };
   $scope.cards = [{
     title: 'Convene and engage community',
     status: 'SUCCESS',
+    member: 'Abimbola Idowu',
     tasks: ['Double the number of major events produced from previous year', 'Craft a 3-minute stump speech', 'Craft a 5-minutes stump speech', 'Double high level engagment iwth twice-monthly email communications']
   }, {
     title: 'book speaking roles to drive income leads',
     status: 'SUCCESS',
+    member: 'Abimbola Idowu',
     tasks: ['Double the number of major events produced from previous year', 'Craft a 3-minute stump speech', 'Craft a 5-minutes stump speech', 'Double high level engagment iwth twice-monthly email communications']
   }, {
     title: 'book speaking roles to drive income leads',
     status: 'SUCCESS',
+    member: 'Nad Enegesi',
     tasks: ['Double the number of major events produced from previous year', 'Craft a 3-minute stump speech', 'Craft a 5-minutes stump speech', 'Double high level engagment iwth twice-monthly email communications']
   }, {
     title: 'book speaking roles to drive income leads',
     status: 'SUCCESS',
+    member: 'Fadekemi Ogunwa',
     tasks: ['Double the number of major events produced from previous year', 'Craft a 3-minute stump speech', 'Craft a 5-minutes stump speech', 'Double high level engagment iwth twice-monthly email communications']
   }, {
     title: 'book speaking roles to drive income leads',
     status: 'SUCCESS',
+    member: 'Obie Fernandez',
     tasks: ['Double the number of major events produced from previous year', 'Craft a 3-minute stump speech', 'Craft a 5-minutes stump speech', 'Double high level engagment iwth twice-monthly email communications']
   }];
 
