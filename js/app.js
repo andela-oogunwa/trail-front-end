cb9b4c06a8ce48ca9e83fbbd81ec6ffadbfb3e86
Abimbola Idowu <abimbola2010@gmail.com>
2015-06-09 14:19:58 +0100

    implement dropdown toggle on cards

--- js/app.js
+++ js/app.js
@@ -12,25 +12,30 @@ angular.module('TrailApp').controller('MainCtrl', ['$scope', '$mdSidenav', funct
     title: 'book speaking roles to drive income leads',
     status: 'SUCCESS',
     tasks: ['Double the number of major events produced from previous year', 'Craft a 3-minute stump speech', 'Craft a 5-minutes stump speech', 'Double high level engagment iwth twice-monthly email communications']
+  }, {
+    title: 'book speaking roles to drive income leads',
+    status: 'SUCCESS',
+    tasks: ['Double the number of major events produced from previous year', 'Craft a 3-minute stump speech', 'Craft a 5-minutes stump speech', 'Double high level engagment iwth twice-monthly email communications']
+  }, {
+    title: 'book speaking roles to drive income leads',
+    status: 'SUCCESS',
+    tasks: ['Double the number of major events produced from previous year', 'Craft a 3-minute stump speech', 'Craft a 5-minutes stump speech', 'Double high level engagment iwth twice-monthly email communications']
+  }, {
+    title: 'book speaking roles to drive income leads',
+    status: 'SUCCESS',
+    tasks: ['Double the number of major events produced from previous year', 'Craft a 3-minute stump speech', 'Craft a 5-minutes stump speech', 'Double high level engagment iwth twice-monthly email communications']
   }];
+
   $scope.toggleSidenav = function(menuId) {
     $mdSidenav(menuId).toggle();
   };
-  $scope.showDropdown = {
-    sectionOne : false,
-    sectionTwo : false,
-    sectionThree : false,
-    sectionFour : false
-  };
 
-  $scope.togglesection = function(sectionName) {
-    console.log('index');
-
-    $scope.showDropdown[sectionName] = !$scope.showDropdown[sectionName];
+  $scope.toggleCards = function(index) {
+    $scope.cards[index].isOpen = !$scope.cards[index].isOpen;
+    angular.forEach($scope.cards, function(value, key) {
+      if (key !== index) {
+        value.isOpen = false;
+      }
+    });
   };
-  $scope.checkDropdown = function(sectionName) {
-    console.log('show dropdown',sectionName);
-    return $scope.showDropdown[sectionName];
-  };
-
 }]);
