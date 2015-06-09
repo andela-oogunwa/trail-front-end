5ee6d8d19929ec3bf15618c9eb0131ed933ae7d3
Abimbola Idowu <abimbola2010@gmail.com>
2015-06-09 14:18:51 +0100

    refactor data to angular controller

--- js/app.js
+++ js/app.js
@@ -4,7 +4,15 @@
 angular.module('TrailApp', ['ngMaterial']);
 
 angular.module('TrailApp').controller('MainCtrl', ['$scope', '$mdSidenav', function($scope, $mdSidenav){
-
+  $scope.cards = [{
+    title: 'Convene and engage community',
+    status: 'SUCCESS',
+    tasks: ['Double the number of major events produced from previous year', 'Craft a 3-minute stump speech', 'Craft a 5-minutes stump speech', 'Double high level engagment iwth twice-monthly email communications']
+  }, {
+    title: 'book speaking roles to drive income leads',
+    status: 'SUCCESS',
+    tasks: ['Double the number of major events produced from previous year', 'Craft a 3-minute stump speech', 'Craft a 5-minutes stump speech', 'Double high level engagment iwth twice-monthly email communications']
+  }];
   $scope.toggleSidenav = function(menuId) {
     $mdSidenav(menuId).toggle();
   };
@@ -16,6 +24,7 @@ angular.module('TrailApp').controller('MainCtrl', ['$scope', '$mdSidenav', funct
   };
 
   $scope.togglesection = function(sectionName) {
+    console.log('index');
 
     $scope.showDropdown[sectionName] = !$scope.showDropdown[sectionName];
   };
