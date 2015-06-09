7d63633b13202ca744b82ae4ae9a067be0f36ca5
Abimbola Idowu <abimbola2010@gmail.com>
2015-06-09 14:30:33 +0100

    fix issue with layout, remove sidenav

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
 
 angular.module('TrailApp', ['ngMaterial']);
 
-angular.module('TrailApp').controller('MainCtrl', ['$scope', '$mdSidenav', function($scope, $mdSidenav){
+angular.module('TrailApp').controller('MainCtrl', ['$scope', function($scope){
   $scope.cards = [{
     title: 'Convene and engage community',
     status: 'SUCCESS',
@@ -25,10 +25,6 @@ angular.module('TrailApp').controller('MainCtrl', ['$scope', '$mdSidenav', funct
     status: 'SUCCESS',
     tasks: ['Double the number of major events produced from previous year', 'Craft a 3-minute stump speech', 'Craft a 5-minutes stump speech', 'Double high level engagment iwth twice-monthly email communications']
   }];
-
-  $scope.toggleSidenav = function(menuId) {
-    $mdSidenav(menuId).toggle();
-  };
 
   $scope.toggleCards = function(index) {
     $scope.cards[index].isOpen = !$scope.cards[index].isOpen;
