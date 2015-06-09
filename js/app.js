8a0c9ccda6e1c1c0ff54362611053dd347332bfe
andela-oogunwa <fadekemi.ogunwa@andela.co>
2015-06-09 02:09:55 +0100

    make front end okr list with status

--- js/app.js
+++ js/app.js
@@ -4,5 +4,20 @@ app.controller('MainCtrl', ['$scope', '$mdSidenav', function($scope, $mdSidenav)
   $scope.toggleSidenav = function(menuId) {
     $mdSidenav(menuId).toggle();
   };
+  $scope.showDropdown = {
+    sectionOne : false,
+    sectionTwo : false,
+    sectionThree : false,
+    sectionFour : false
+  };
+
+  $scope.togglesection = function(sectionName) {
+    
+    $scope.showDropdown[sectionName] = !$scope.showDropdown[sectionName];
+  };
+  $scope.checkDropdown = function(sectionName) {
+    console.log('show dropdown',sectionName);
+    return $scope.showDropdown[sectionName];
+  };
  
 }]);
